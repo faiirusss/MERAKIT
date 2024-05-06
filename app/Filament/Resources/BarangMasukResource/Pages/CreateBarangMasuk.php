d3260030e14188a1652024245309144360f71a9c
faiirusss <87980888+faiirusss@users.noreply.github.com>
2024-05-06 16:23:50 +0700

    edit/barangMasukResource

--- app/Filament/Resources/BarangMasukResource/Pages/CreateBarangMasuk.php
+++ app/Filament/Resources/BarangMasukResource/Pages/CreateBarangMasuk.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateBarangMasuk extends CreateRecord
 {
     protected static string $resource = BarangMasukResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
